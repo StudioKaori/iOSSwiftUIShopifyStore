b5a95371b579783080afce3ca179200c952e2073
Studio Kaori <studiokaori.com@gmail.com>
2022-05-30 08:29:12 +0200

    Initial Commit

---
+++ SwiftUIShopifyStore/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIShopifyStore
+//
+//  Created by Kaori Persson on 2022-05-30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIShopifyStore/SwiftUIShopifyStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIShopifyStoreApp.swift
+//  SwiftUIShopifyStore
+//
+//  Created by Kaori Persson on 2022-05-30.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIShopifyStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
