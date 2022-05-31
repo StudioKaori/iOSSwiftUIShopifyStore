87f1dff7e2b7465b1fdf9e77dc8aaccc3b91c9f3
Studio Kaori <studiokaori.com@gmail.com>
2022-05-31 12:49:08 +0200

    Add: ProductCardView

--- SwiftUIShopifyStore/Views/Product List/ProductCardView.swift
+++ SwiftUIShopifyStore/Views/Product List/ProductCardView.swift
@@ -8,13 +8,20 @@
 import SwiftUI
 
 struct ProductCardView: View {
+    // MARK: - Property
+    
+    let product: Product
+    
+    // MARK: - Body
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text(product.title)
     }
 }
 
 struct ProductCardView_Previews: PreviewProvider {
     static var previews: some View {
-        ProductCardView()
+        ProductCardView(product: previewSampleProductObject)
+            .previewLayout(.fixed(width: 200, height: 300))
+            .padding()
     }
 }
