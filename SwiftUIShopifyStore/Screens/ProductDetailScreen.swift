1984a624bf94a25fa988bb7885fdf7da1f97bd49
Studio Kaori <studiokaori.com@gmail.com>
2022-05-31 06:47:20 +0200

    Add: product detail screen

---
+++ SwiftUIShopifyStore/Screens/ProductDetailScreen.swift
@@ -0,0 +1,20 @@
+//
+//  ProductDetailScreen.swift
+//  SwiftUIShopifyStore
+//
+//  Created by Kaori Persson on 2022-05-31.
+//
+
+import SwiftUI
+
+struct ProductDetailScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ProductDetailScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        ProductDetailScreen()
+    }
+}
