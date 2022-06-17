bcaeecc7767e5e0219fb1b090b378d24504c3104
Studio Kaori <studiokaori.com@gmail.com>
2022-06-17 12:18:21 +0200

    Add: playerView

--- SwiftUIShopifyStore/Screens/HomeScreen.swift
+++ SwiftUIShopifyStore/Screens/HomeScreen.swift
@@ -6,46 +6,6 @@
 //
 
 import SwiftUI
-import AVKit
-import AVFoundation
-
-struct PlayerView: UIViewRepresentable {
-    func updateUIView(_ uiView: UIView, context: UIViewRepresentableContext<PlayerView>) {
-    }
-
-    func makeUIView(context: Context) -> UIView {
-        return LoopingPlayerUIView(frame: .zero)
-    }
-}
-
-
-class LoopingPlayerUIView: UIView {
-    private let playerLayer = AVPlayerLayer()
-    private var playerLooper: AVPlayerLooper?
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        // Load the resource -> h
-        let fileUrl = Bundle.main.url(forResource: "main-s", withExtension: "mp4")!
-        let asset = AVAsset(url: fileUrl)
-        let item = AVPlayerItem(asset: asset)
-        // Setup the player
-        let player = AVQueuePlayer()
-        playerLayer.player = player
-        playerLayer.videoGravity = .resizeAspectFill
-        layer.addSublayer(playerLayer)
-        // Create a new player looper with the queue player and template item
-        playerLooper = AVPlayerLooper(player: player, templateItem: item)
-        // Start the movie
-        player.play()
-    }
-    override func layoutSubviews() {
-        super.layoutSubviews()
-        playerLayer.frame = bounds
-    }
-}
 
 struct HomeScreen: View {
     // MARK: - property
@@ -54,8 +14,8 @@ struct HomeScreen: View {
     // MARK: - Body
     var body: some View {
         NavigationView {
-            VStack {
-                
+            
+            ZStack {
                 GeometryReader{ geo in
                     PlayerView()
                         .aspectRatio(contentMode: .fill)
@@ -64,21 +24,25 @@ struct HomeScreen: View {
                         .overlay(Color.black.opacity(0.2))
                         .blur(radius: 1)
                         .edgesIgnoringSafeArea(.all)
-                }
+                }  //: video
                 
-                Text("KAORI")
-                    .font(Font.logoFont(size: 88))
-                
-                Text(shopInfoViewModel.shopInfo.name)
-                    .font(Font.logoFont(size: 18))
-                    .padding()
-                
-                ProductListScreen()
+                VStack {
 
-            } //: VStack
-            .onAppear{
-                shopInfoViewModel.getShopInfo()
+                    Text("KAORI")
+                        .font(Font.logoFont(size: 88))
+                    
+                    Text(shopInfoViewModel.shopInfo.name)
+                        .font(Font.logoFont(size: 18))
+                        .padding()
+                    
+                    ProductListScreen()
+
+                } //: VStack
+                .onAppear{
+                    shopInfoViewModel.getShopInfo()
+                }
             }
+            
         }
     }
 }
