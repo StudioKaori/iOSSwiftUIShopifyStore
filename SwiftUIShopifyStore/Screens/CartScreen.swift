293ee39094870a394eb5c362326103bc572ee4cb
Studio Kaori <studiokaori.com@gmail.com>
2022-06-19 09:41:32 +0200

    Style:cart screen

--- SwiftUIShopifyStore/Screens/CartScreen.swift
+++ SwiftUIShopifyStore/Screens/CartScreen.swift
@@ -6,51 +6,68 @@
 //
 
 import SwiftUI
+import MobileBuySDK
 
 struct CartScreen: View {
     @EnvironmentObject var cartItems: CartItems
     
     var body: some View {
-        VStack {
+        VStack (alignment: .leading) {
             Text("Cart")
             
-            ForEach(cartItems.cartItems) { product in
-                NavigationLink(destination: ProductDetailScreen(product: product)) {
-                    HStack {
-                        URLImage(url: product.imageUrls[0])
-                        .frame(width: 70, height: 70)
-                        .background(Color.gray)
-                        .cornerRadius(4)
-                        
-                        VStack(alignment: .leading, spacing: 3) {
-                            Text(product.title)
-                                .bold()
+            ScrollView {
+                ForEach(cartItems.cartItems) { product in
+                    NavigationLink(destination: ProductDetailScreen(product: product)) {
+                        HStack(alignment: .top, spacing: 4) {
+                            URLImage(url: product.imageUrls[0])
+                            .frame(width: 100, height: 100)
                             
-                            HStack {
-//                                Text(tvShow.show.premiered?.prefix(4).appending(" -") ?? "")
-//                                    .font(.footnote)
-//
-//                                Text(tvShow.show.ended?.prefix(4) ?? "")
-//                                    .font(.footnote)
+                            VStack(alignment: .leading, spacing: 4) {
                                 
-                                Spacer()
+                                Text(product.title)
+                                    .foregroundColor(.black)
+                                    .font(.logoFont(size: 24))
                                 
-                                Image(systemName: "star.fill")
-                                    .font(.footnote)
-                                    .foregroundColor(Color.gray)
-                                
-                                Text("\(cartItems.quantityOfCartItems[product.handle] ?? 0)")
-                                    .font(.footnote)
-                            }
-                        }
+                                HStack(alignment: .top) {
+                                    
+                                    Button(action: {
+            
+                                    }, label: {
+                                      Image(systemName: "minus.circle")
+                                    })
+                                    
+                                    Text("\(cartItems.quantityOfCartItems[product.handle] ?? 0)")
+                                    
+                                    Button(action: {
 
+                                    }, label: {
+                                      Image(systemName: "plus.circle")
+                                    })
+                                    
+                                    
+                                    Spacer()
+                                    
+                                    Image(systemName: "trash")
+                                        
+                                    
+                                }
+                                .font(.logoFont(size: 24))
+                                .foregroundColor(Color.gray)
+                                .padding(.horizontal, 6)
+                                .padding(.vertical, 0)
+                            }
+
+                        }
                     }
-                }
-                .padding(.horizontal, 2)
-                .padding(.vertical, 6)
-                 //: navigation link
-                
-            } //: foreach
+                    .padding(.horizontal, 2)
+                    .padding(.vertical, 6)
+                     //: navigation link
+                    
+                    Divider()
+                    
+                } //: foreach
+            }
+
         } //: Vstack
                     
     }
