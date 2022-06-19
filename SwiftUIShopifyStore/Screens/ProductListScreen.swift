42ac0759f057a0c4dde91acc329c67a1fefb0a46
Studio Kaori <studiokaori.com@gmail.com>
2022-06-19 21:37:00 +0200

    Style: search text field

--- SwiftUIShopifyStore/Screens/ProductListScreen.swift
+++ SwiftUIShopifyStore/Screens/ProductListScreen.swift
@@ -10,14 +10,35 @@ import SwiftUI
 struct ProductListScreen: View {
     // MARK: - property
     @StateObject private var productListViewModel = ProductListViewModel()
+    @State private var productSearchQuery = ""
     
     var columns: [GridItem] = Array(repeating: .init(.flexible(), spacing: 10), count: 2)
     
     // MARK: - body
     var body: some View {
         VStack {
-            Text("Products")
-                .font(.logoFont(size: 32))
+            
+            // title and searchbar
+            HStack {
+                Text("Products")
+                    .font(.logoFont(size: 32))
+                
+                HStack {
+                    Image(systemName: "magnifyingglass")
+                        .foregroundColor(.gray)
+                    TextField("Search...", text: $productSearchQuery)
+                        .keyboardType(.asciiCapable)
+                        
+                }
+                .frame(width: UIScreen.main.bounds.width - 160)
+                .padding(8)
+                .overlay(
+                        RoundedRectangle(cornerRadius: 4)
+                            .stroke(.gray, lineWidth: 1)
+                    )
+  
+            }  //: title and searchbar
+            
             
             ScrollView {
 
