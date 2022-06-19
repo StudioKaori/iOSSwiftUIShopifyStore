5c548bc83c0e9bd55e3c371258f94d59d7c38dfe
Studio Kaori <studiokaori.com@gmail.com>
2022-06-19 09:49:24 +0200

    Refactor: separate cartItemView from cart screen

--- SwiftUIShopifyStore/Screens/CartScreen.swift
+++ SwiftUIShopifyStore/Screens/CartScreen.swift
@@ -9,7 +9,7 @@ import SwiftUI
 import MobileBuySDK
 
 struct CartScreen: View {
-    @EnvironmentObject var cartItems: CartItems
+
     
     var body: some View {
         VStack (alignment: .center) {
@@ -17,61 +17,7 @@ struct CartScreen: View {
                 .font(.logoFont(size: 32))
             Divider()
             
-            // MARK: - Cart Items
-            ScrollView {
-                ForEach(cartItems.cartItems) { product in
-                    NavigationLink(destination: ProductDetailScreen(product: product)) {
-                        HStack(alignment: .top, spacing: 4) {
-                            URLImage(url: product.imageUrls[0])
-                            .frame(width: 100, height: 100)
-                            
-                            VStack(alignment: .leading, spacing: 4) {
-                                
-                                Text(product.title)
-                                    .foregroundColor(.black)
-                                    .font(.logoFont(size: 24))
-                                
-                                Spacer()
-                                
-                                HStack(alignment: .top) {
-                                    
-                                    Button(action: {
-            
-                                    }, label: {
-                                      Image(systemName: "minus.circle")
-                                    })
-                                    
-                                    Text("\(cartItems.quantityOfCartItems[product.handle] ?? 0)")
-                                    
-                                    Button(action: {
-
-                                    }, label: {
-                                      Image(systemName: "plus.circle")
-                                    })
-                                    
-                                    
-                                    Spacer()
-                                    
-                                    Image(systemName: "trash")
-                                        
-                                    
-                                }
-                                .font(.logoFont(size: 24))
-                                .foregroundColor(Color.gray)
-                                .padding(.horizontal, 6)
-                                .padding(.vertical, 0)
-                            }
-
-                        }
-                    }
-                    .padding(.horizontal, 2)
-                    .padding(.vertical, 6)
-                     //: navigation link
-                    
-                    Divider()
-                    
-                } //: foreach
-            }
+            CartItemsView()
             
             Spacer()
             BottomMenuBarView(currentView: "CartScreen")
