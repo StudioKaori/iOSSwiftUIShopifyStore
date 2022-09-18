38eea53c04670dd5fa139502e2b2ac42295786ea
Kaori <67104053+StudioKaori@users.noreply.github.com>
2022-09-18 11:28:10 +0200

    Add: getProductDetail

---
+++ SwiftUIShopifyStore/ViewModels/ProductViewModel.swift
@@ -0,0 +1,89 @@
+//
+//  ProductViewModel.swift
+//  SwiftUIShopifyStore
+//
+//  Created by Kaori Persson on 2022-09-18.
+//
+
+import Foundation
+import SwiftUI
+import MobileBuySDK
+
+/// ViewModel for getting Product information from Shopify backend.
+class ProductViewModel: ObservableObject {
+    
+    // MARK: - Properties
+    @Published var product: Product?
+    
+    // MARK: - Methods
+    /// Get product information from Shopify backend.
+    ///  To specify the category, set 'tag:tagname' in the query parameter.
+    func getProductDetail(productHandle: String) {
+        // products information
+        // https://shopify.dev/api/storefront/2022-04/queries/products
+        // product field : https://shopify.dev/api/storefront/2022-04/objects/Product#fields
+        // buy sdk: https://github.com/Shopify/mobile-buy-sdk-ios/
+        let productsQuery = Storefront.buildQuery { $0
+            .product(handle: productHandle) { $0
+                .handle()
+                .title()
+                .description()
+                .images(first: 5) { $0
+                    .edges { $0
+                        .node { $0
+                            .url()
+                        }
+                    }
+                }
+                .priceRange { $0
+                    .maxVariantPrice { $0
+                        .amount()
+                        .currencyCode()
+                    }
+                    .minVariantPrice{ $0
+                        .amount()
+                        .currencyCode()
+                    }
+                }
+            }
+        }
+        
+        let productsTask = ShopifyClient.client.queryGraphWith(productsQuery) { [weak self] response, error in
+            
+            // Unwrap response data
+            guard let data = response else {
+                print("Query GraphQL error in getProductInfo")
+                if let error = error {
+                    print("Query GraphQL error: \(error)")
+                }
+                return
+            }
+            
+            print("product data", data)
+            
+//
+//                let images: [URL] = data.images.edges.map {
+//                    $0.node.url
+//                }
+//
+//                let product: Product = Product(
+//                    title: data.node.title,
+//                    description: data.node.description,
+//                    price: data.node.priceRange.maxVariantPrice.amount,
+//                    imageUrls: images,
+//                    handle: data.node.handle
+//                )
+//
+//
+//            //print("Product View Model : \(products)")
+//            DispatchQueue.main.async {
+//                self?.product = product
+//            }
+        }
+        productsTask.resume()
+        
+    } //:getProductDetail
+    
+}
+
+
